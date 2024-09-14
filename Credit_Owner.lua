d54ba0adaa32c85bfeec23e32eebc6ba13e4baaf
Smorgasbord <90550597+MIKEDRIPZOWSKU@users.noreply.github.com>
2024-09-14 20:29:28 +0800

    Create Credit_Owner.lua

---
+++ Credit_Owner.lua
@@ -0,0 +1,11 @@
+getgenv().CreditName = {
+DisplayYoutube = "YT @toberclock877",
+SourceYoutube = "ALWAYS GIVE CREDITS! Discord Server >> https://discord.gg/zH2ax5ffbs"
+}
+
+task.spawn(function()
+wait(5)
+if game.CoreGui:FindFirstChild("Death_Gui") then
+game.Players.LocalPlayer:kick("Nuh uh, Keep using cracked script i will replace the script to ban script.")
+end
+end)
